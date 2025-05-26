c0a1d4a966fcbebd0d416b30f275d689b20264d1
Tim <techwithtimm@gmail.com>
2025-05-26 17:52:18 +0400

    update proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log(process.env.DEBUG)
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      }
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  console.log('Environment variables:', env.VITE_DEBUG)
+  
+  return {
+    plugins: [react()],
+    server: {
+      ...(env.VITE_DEBUG === 'true' && {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:8000',
+            changeOrigin: true,
+            secure: false,
+          }
+        }
+      })
     }
   }
 })
